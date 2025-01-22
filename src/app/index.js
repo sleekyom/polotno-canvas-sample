39d407bdc35f7654cf3d3ca78384b6d8be2d96c1
sleekyom <amdas72@gmail.com>
2025-01-22 22:06:33 +0200

    Add dynamic editor component and update layout structure

---
+++ src/app/index.js
@@ -0,0 +1,20 @@
+'use client'
+
+import dynamic from 'next/dynamic';
+const Editor = dynamic(() => import('../components/editor.js'), {
+  ssr: false,
+});
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Editor />
+      <style jsx global>{`
+        body {
+          padding: 0;
+          margin: 0;
+        }
+      `}</style>
+    </div>
+  );
+}
